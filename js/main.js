3116294521a5e81bb64c1b7fcdd6187a38944467
killgram <killgram94@gmail.com>
2020-10-12 17:41:15 +0700

    Реализовал функции создания отдельных блоков формы, реализовал поле name

--- js/main.js
+++ js/main.js
@@ -21,25 +21,71 @@ inputFile.addEventListener('change', function () {
 
 createbtn.onclick = async function (e) {
     if (inputLink.value == '') {
-        createForm(inputData);
+        createForm();
     } else {
         let response = await fetch(inputLink.value);
         inputData = await response.json();
-        createForm(inputData);
+        createForm();
     }
 }
 
-// console.log(inputData);
-
 // create form
-function createForm(inputData) {
+function createForm() {
     let divForm2 = document.createElement('div');
-    divForm2.setAttribute('class','container-fluid');
+    divForm2.setAttribute('class', 'container-fluid');
     divForm1.after(divForm2);
     //render main part
     let renderForm = document.createElement('form');
-    renderForm.setAttribute('class','col-lg-6 col-auto justify-content-center mx-auto');
-    renderForm.setAttribute('id','renderForm');
-    renderForm.setAttribute('name','renderForm');
+    renderForm.setAttribute('class', 'col-lg-6 col-auto justify-content-center mx-auto');
+    renderForm.setAttribute('id', 'renderForm');
     divForm2.append(renderForm);
+    let fieldset = document.createElement('fieldset');
+    renderForm.append(fieldset);
+    //render blocks
+    createBlocks(inputData);
+}
+
+//create blocks
+function createBlocks(data) {
+    for (i = 0; i < Object.entries(data).length; i++) {
+        let formBlock = Object.entries(data)[i];
+        renderBlocks(formBlock);
+    }
+}
+
+//render main blocks
+function renderBlocks(block) {
+    if (block.includes("name")){
+        renderName(block);
+    }
+    if (block.includes("fields")){
+        renderFields(block);
+    }
+    if (block.includes("references")){
+        renderReferences(block);
+    }
+    if(block.includes("buttons")){
+        renderButtons(block);
+    }
+}
+
+//render name
+function renderName(name){
+    let formName = Object.values(name)[1];
+    renderForm.setAttribute('name', formName);
+}
+
+//render fields
+function renderFields(field){
+    console.log(field);
+}
+
+//render references
+function renderReferences(references) {
+    console.log(references);
+}
+
+//render buttons
+function renderButtons(buttons) {
+    console.log(buttons);
 }
