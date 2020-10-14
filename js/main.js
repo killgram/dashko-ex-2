3961f5bd8e5785396e6f0a08f69d4e8b7c24ef33
killgram <killgram94@gmail.com>
2020-10-15 01:56:24 +0700

    Финальные правки, добавил комментарии

--- js/main.js
+++ js/main.js
@@ -5,12 +5,6 @@ let createbtn = document.getElementById('createForm');
 let clearbtn = document.getElementById('clearForm');
 let inputLink = document.getElementById('inputWay');
 let inputFile = document.getElementById('inputFile');
-// inputLink.value = 'https://raw.githubusercontent.com/killgram/JSON-format/master/addpost.js';
-// inputLink.value = 'https://raw.githubusercontent.com/killgram/JSON-format/master/colorsheme.js';
-// inputLink.value = 'https://raw.githubusercontent.com/killgram/JSON-format/master/interview.js';
-// inputLink.value = 'https://raw.githubusercontent.com/killgram/JSON-format/master/signin.js';
-// inputLink.value = 'https://raw.githubusercontent.com/killgram/JSON-format/master/signup.js';
-
 
 let inputData;
 // get data
@@ -45,6 +39,7 @@ clearbtn.onclick = function () {
 // create form
 function createForm() {
     if (!inputData) {
+        alert('Надо бы выбрать JSON');
         return;
     }
     createbtn.setAttribute('disabled', 'true');
